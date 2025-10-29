9d8810d093df44c8b0f49049ae1c97a4cd050ce2
Patraxx <patrik.cederlund@gmail.com>
2025-10-29 14:06:14 +0100

    nya samples

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,21 @@
 AudioGeneratorWAV *wav;
 AudioFileSourcePROGMEM *file2;
 AudioOutputI2S *out;
+#define vibration_pin 27
 
 void setup() {
+  pinMode(vibration_pin, OUTPUT);
+  digitalWrite(vibration_pin, LOW);
   Serial.begin(115200);
   delay(1000);
   Serial.println("Start");
 
-  file2 = new AudioFileSourcePROGMEM(pulse_16_bit, pulse_16_bit_length);
+  //file2 = new AudioFileSourcePROGMEM(pulse_16_bit, pulse_16_bit_length);
+  file2= new AudioFileSourcePROGMEM(normal_pulse, normal_pulse_length);
   wav = new AudioGeneratorWAV();
   out = new AudioOutputI2S();
   out->SetPinout(26, 25, 22);
   out->SetGain(0.8); // 0.0 .. 4.0
-  wav->begin(file2, out);
   xTaskCreate(
       triggerTask,          // Task function
       "Trigger Task",       // Name of the task (for debugging)
@@ -43,6 +46,8 @@ void loop() {
     file2 = new AudioFileSourcePROGMEM(pulse_16_bit, pulse_16_bit_length);
     wav = new AudioGeneratorWAV();
     wav->begin(file2, out);
+    digitalWrite(vibration_pin, HIGH);
+
   }
 
   // Keep playback running until the WAV finishes
@@ -52,6 +57,7 @@ void loop() {
     // finished playing, clean up
     delete wav; wav = nullptr;
     delete file2; file2 = nullptr;
+    digitalWrite(vibration_pin, LOW);
     Serial.println("Playback finished.");
   }
 }
